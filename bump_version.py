2a4392a1e4775e84331fbe944dd8f4772d1cd05d
Taiki Iwamura <takki.0206@gmail.com>
2022-03-26 09:50:26 +0000

    Write bump_version.py

--- bump_version.py
+++ bump_version.py
@@ -1,2 +1,27 @@
-def cli():
-    print("Hello World!")
+import glob
+import re
+from itertools import chain
+
+import click
+
+
+@click.command()
+@click.argument("new_version_num", nargs=1)
+def cli(new_version_num):
+    """This script changes package's version number."""
+    setup_files = chain(
+        glob.glob("**/setup.py", recursive=True),
+        glob.glob("**/setup.cfg", recursive=True),
+    )
+    for setup_file in setup_files:
+        with open(setup_file, "r") as f:
+            lines = f.readlines()
+        with open(setup_file, "w") as f:
+            new_lines = [
+                re.sub(r"[0-9]+.[0-9]+.[0-9]+", new_version_num, line)
+                if "version" in line
+                else line
+                for line in lines
+            ]
+            for line in new_lines:
+                f.write(line)
